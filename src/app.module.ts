ba5844237c339294aff7cbc6d75ba819ceed832f
mitesh darda <mitesh.darda.0@gmail.com>
2024-07-20 13:19:26 +0530

    feat: integrate config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import config from './config/configuration';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      load: [config],
     }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       global: true,
     }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     DatabaseModule,
   ],
   controllers: [AppController],
